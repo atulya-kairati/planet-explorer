9012c0bd0b0a1be86f8eabce4d8459afbeeedfc9
atulya-kairati <atulyakairati@gmail.com>
2022-12-17 23:44:06 +0530

    found planets

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-const parse = require("csv-parse")
+const { parse } = require("csv-parse")
 const fs = require("fs")
 
 const fileStream = fs.createReadStream("./data/KOI_data.csv")
 
-fileStream.on("data", (data) => {
-  console.log(data)
-  console.log('----------------------')
-});
+const habitablePlanets = []
+
+/**
+ * [pipe()]: 
+ * Connects readable stream source
+ * to writable stream destination.
+ */
+fileStream.pipe(parse({
+  comment: '#',
+  columns: true  // This will return a row in form of a k:v pair
+}))
+  .on("data", (data) => {
+    if(isHabitable(data)){
+      habitablePlanets.push(data)
+    }
+  });
 
 fileStream.on('end', () => {
-	console.log("--File end--")
+  console.log(`${habitablePlanets.length} habitable planets found!`)
+  console.log(habitablePlanets.map((planet) => planet['kepler_name']));
+  console.log("--Fin--")
 })
 
 fileStream.on('error', (err) => {
-	console.log(err)
+  console.log(err)
 })
+
+const isHabitable = (planet) => 
+  planet['koi_disposition'] === "CONFIRMED" &&
+  planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && // Stellar flux limit
+  planet['koi_prad'] < 1.6  // Radii limit in relation to Earth radii 
